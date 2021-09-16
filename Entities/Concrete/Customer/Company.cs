e65caae41ddaed58da101b0c05a3d4212eb06e0d
SevtapE <sevtaperbek@outlook.com>
2021-09-16 17:44:33 +0300

    Add project files.

---
+++ Entities/Concrete/Customer/Company.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.Customer
+{
+    public class Company:IEntity,ICustomer
+    {
+    }
+}
