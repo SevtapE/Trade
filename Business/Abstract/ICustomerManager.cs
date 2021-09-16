b82fca648559f5c5bc74880eef8d4396e5bf389b
SevtapE <sevtaperbek@outlook.com>
2021-09-16 19:52:06 +0300

    The Business is created

---
+++ Business/Abstract/ICustomerManager.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+   public interface ICustomerManager
+    {
+        void Add(ICustomer customer);
+        void Update(ICustomer customer);
+        void Delete(ICustomer customer);
+        List<ICustomer> List();
+    }
+}
---
+++ Business/Concrete/Customer/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete.Customer
+{
+    public class CustomerManager : ICustomerManager
+    {
+        public void Add(ICustomer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ICustomer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ICustomer> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ICustomer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
